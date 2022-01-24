7b50224471a14212e558731d74d1312486c4f978
Anselm Eicke <32741755+anselm-eicke@users.noreply.github.com>
2022-01-24 15:34:31 +0100

    results

--- Two node models/225a/gamsnext.cmd
+++
@@ -1,2 +0,0 @@
-@echo off
-gmsgennt.cmd "C:\Users\a.eicke\Documents\Locational-investment-signals\Two node models\225a\" "C:\Users\a.eicke\Documents\Locational-investment-signals\Two node models\" %1 "C:\Users\a.eicke\Documents\Locational-investment-signals\Two node models\225a\gamscntr.dat" "C:\GAMS\win64\30.3\" "CPLEX"
