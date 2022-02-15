1dd9f3f4c44f619444f321c9ad7fc984af77b49b
Anselm Eicke <32741755+anselm-eicke@users.noreply.github.com>
2022-02-15 15:58:01 +0100

    with agnostic

--- Two node models/225c/gamsnext.cmd
+++
@@ -1,2 +0,0 @@
-@echo off
-gmsgennt.cmd "C:\Users\a.eicke\Documents\Locational-investment-signals\Two node models\225c\" "C:\Users\a.eicke\Documents\Locational-investment-signals\Two node models\" %1 "C:\Users\a.eicke\Documents\Locational-investment-signals\Two node models\225c\gamscntr.dat" "C:\GAMS\win64\30.3\" "CPLEX"
